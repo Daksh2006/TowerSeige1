8458966ee77dfece3b2f9b07a2f1d8cafdc768f6
Daksh2006 <68325624+Daksh2006@users.noreply.github.com>
2020-09-08 09:43:59 +0530

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,7 +1,7 @@
 class Player extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
-    this.image = loadImage("sprites/images.png");
+    this.image = loadImage("images.png");
   }
 
   display() {
